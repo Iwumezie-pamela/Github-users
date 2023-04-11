740120ca7c6bbf164ab9466cd5d3941f3fc46413
iwumezie Pamela <iwumeziep@gmail.com>
2023-04-11 18:01:15 +0100

    completed the basic structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
-import { useState } from 'react';
+import React from 'react';
+import Home from './components/Home';
 
 function App() {
   return (
     <div className='App'>
-      <h1 className='text-3xl font-bold underline'>Hello world!</h1>
+      <Home />
     </div>
   );
 }
